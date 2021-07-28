3961f0c8d06a87074921d32eb4dbe8f86105e982
Minhdory <87761615+Minhdory@users.noreply.github.com>
2021-07-28 13:25:13 +0700

    Update b5.js

--- b5.js
+++ b5.js
@@ -1,1 +1,14 @@
-function IsSpecialNumber(num)
+function IsSpecialNumber(num){
+if (num>0 && num%2 === 0){
+  let uocChan = 1; uocLe = 1
+  for(i=1; i<num; i++){
+    if(n%i === 0){
+      if(i%2 === 0){uocChan++};
+      else uocLe++
+    }
+  }
+if(uocChan===uocLe){
+  return "true";
+} else return "false";
+}
+console.log(IsSpecialNumber(123456))
