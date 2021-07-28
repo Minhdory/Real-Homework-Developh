855fd1158a888784a4e3c9a49fb338725556ec4e
Minhdory <87761615+Minhdory@users.noreply.github.com>
2021-07-28 12:16:23 +0700

    Update b4.js

--- b4.js
+++ b4.js
@@ -1,9 +1,10 @@
 function divisibility(a, b){
   let i=0
-  while(a%b !== 0){
-    a++
-    i++
-    console.log(i)
+  while (a%b !== 0){
+    a++;
+    i++;
   }
+  return(i);
 }
 console.log(divisibility(1, 5))
+
